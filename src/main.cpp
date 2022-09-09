5fb69f175f08f35663bc2086cfad5e5b96f655d1
m <mustafagokce009@gmail.com>
2022-09-09 17:22:03 +0300

    Get NMEA lines

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,30 @@
-#include "../include/main.h"
+#include "main.h"
 
 SoftwareSerial mySerial(17, 16);
+const char nmea_start = 0x24;
+const char carriage_return = 0x0D;
+const char line_feed = 0x0A;
+char sentence[79];
+int8_t i = 0;
 
-void setup()
-{
-  Serial.begin(115200);
-  mySerial.begin(9600);
+void setup() {
+    Serial.begin(115200);
+    mySerial.begin(9600);
 }
 
-void loop()
-{
-  if (mySerial.available())
-    Serial.write(mySerial.read());
-  if (Serial.available())
-    mySerial.write(Serial.read());
+void loop() {
+    if (mySerial.available()) {
+        char c = mySerial.read();
+        if (c == nmea_start) {
+            i = 0;
+        } else if (c == carriage_return) {
+            i = 0;
+        } else if (c == line_feed) {
+            i = 0;
+            Serial.println(sentence);
+        } else {
+            sentence[i] = c;
+            i++;
+        }
+    }
 }
